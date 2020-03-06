05919d3171a55ee75a58d2cce308ca6939a36c21
Julian Acosta <8892386+julacosta9@users.noreply.github.com>
2020-03-05 20:59:39 -0800

    Update port

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+
+let PORT = process.env.PORT;
+
+if (PORT == null || PORT == "") {
+  PORT = 8000;
+}
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
